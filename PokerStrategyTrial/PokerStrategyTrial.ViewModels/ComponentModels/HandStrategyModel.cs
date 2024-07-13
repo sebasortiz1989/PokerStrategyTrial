7d64397e829a7567850bece06c6cedd6a44c4a82
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-07-13 12:34:57 -0300

    Sebas: [add] Creating Hand Strategies

--- PokerStrategyTrial/PokerStrategyTrial.ViewModels/ComponentModels/HandStrategyModel.cs
+++ PokerStrategyTrial/PokerStrategyTrial.ViewModels/ComponentModels/HandStrategyModel.cs
@@ -1,8 +1,23 @@
-﻿using PokerStrategyTrial.ViewModels.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Avalonia.Media;
 
 namespace PokerStrategyTrial.ViewModels.ComponentModels;
 
-public class HandStrategyModel : ViewModelBase
+public class HandStrategyModel
 {
+    public HandStrategyModel(string handName, float weight, float[] strategy, Color[] strategyColors)
+    {
+        Hand = handName;
+        Weight = weight;
+        Strategy = strategy;
+        StrategyColors = strategyColors;
+    }
     
+    public string Hand { get; private set; }
+
+    public float Weight { get; private set; }
+
+    public float[] Strategy { get; private set; }
+
+    public Color[] StrategyColors { get; private set; }
 }
--- PokerStrategyTrial/PokerStrategyTrial.ViewModels/ViewModels/TrialTaskViewModel.cs
+++ PokerStrategyTrial/PokerStrategyTrial.ViewModels/ViewModels/TrialTaskViewModel.cs
@@ -1,22 +1,66 @@
-﻿using System.Reactive;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
+using Avalonia.Media;
+using PokerStrategyTrial.ViewModels.ComponentModels;
 using ReactiveUI;
 
 namespace PokerStrategyTrial.ViewModels.ViewModels
 {
     public class TrialTaskViewModel : ViewModelBase
     {
+        private readonly string[] _cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+        private readonly Color[] _colors = [Colors.Aqua, Colors.Red, Colors.Green, Colors.Chocolate, Colors.Cornsilk];
+
         public TrialTaskViewModel()
         {
-            ExampleCommand = ReactiveCommand.Create<object>(Execute);
+            CommandReloadColors = ReactiveCommand.Create<object>(Execute);
+            CreateHandStrategies();
         }
+
+        public ReactiveCommand<object, Unit> CommandReloadColors { get; }
+
+        public ObservableCollection<HandStrategyModel> HandStrategies { get; private set; } = new();
+
+        public string TestOutput { get; set; } = "Clicked Button information";
 
         private void Execute(object parameter)
         {
             TestOutput = "Button Clicked!";
         }
 
-        public string TestOutput { get; set; } = "Clicked Button information";
+        private void CreateHandStrategies()
+        {
+            Random rnd = new Random();
 
-        public ReactiveCommand<object, Unit> ExampleCommand { get; }
+            for (int i = 0; i < _cards.Length; i++)
+            {
+                for (int j = 0; j < _cards.Length; j++)
+                {
+                    int numberOfStrategies = rnd.Next(1, _colors.Length - 1);
+                    Collection<float> strategiesFrequency = [];
+                    float sumOfFrequencies = 0;
+                    for (int z = 0; z < numberOfStrategies; z++)
+                    {
+                        if (z == numberOfStrategies - 1)
+                        {
+                            strategiesFrequency.Add(1 - sumOfFrequencies);
+                            break;
+                        }
+
+                        var newFrequency = rnd.NextSingle() * (1 - sumOfFrequencies);
+                        strategiesFrequency.Add(newFrequency);
+                        sumOfFrequencies += newFrequency;
+                    }
+
+                    HandStrategyModel strategy = new HandStrategyModel(
+                        handName: $"{_cards[i]}{_cards[j]}{(i == j ? string.Empty : i > j ? "o" : "s")}",
+                        rnd.NextSingle(),
+                        strategiesFrequency.ToArray(),
+                        _colors[new Range(0, numberOfStrategies)]);
+
+                    HandStrategies.Add(strategy);
+                }
+            }
+        }
     }
 }
